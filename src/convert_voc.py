7bd325b89842084c4d06b4d0270b6dde66737a76
Daiki.Sanno <daiki.sanno@gmail.com>
2017-09-01 11:10:06 +0900

    Fix PASCAL VOC convert script.

--- src/convert_voc.py
+++ src/convert_voc.py
@@ -1,53 +1,78 @@
 import argparse
-import numpy as np
+import json
 import os
-import six
+import shutil
 from xml.etree import ElementTree
 
-pickle = six.moves.cPickle
 
-object_names = ['aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car',
-                'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse',
-                'motorbike', 'person', 'pottedplant', 'sheep', 'sofa', 'train',
-                'tvmonitor']
-object_ids = {name: i for i, name in enumerate(object_names)}
+def make_dirs(path):
+    if os.path.exists(path):
+        return
+    os.makedirs(path)
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_dir', type=str, help='directory path of attnotation e.g. "VOC2007"')
+    parser.add_argument('output_dir', type=str, help='output directory path')
+    return parser.parse_args()
+
 
 def parse_object(elem):
-    label = object_ids[elem.find('name').text]
+    label = elem.find('name').text
     box_elem = elem.find('bndbox')
     left = int(box_elem.find('xmin').text)
     right = int(box_elem.find('xmax').text)
     top = int(box_elem.find('ymin').text)
     bottom = int(box_elem.find('ymax').text)
-    return (label, (left, top, right, bottom))
+    region = {
+        'category': label,
+        'bbox': [left, top, right - left, bottom - top]
+    }
+    return region
 
-def parse_file(file_path):
+
+def parse_annotation(file_path):
     record = {}
     tree = ElementTree.parse(file_path)
     root = tree.getroot()
-    xml_file = os.path.basename(file_path)
     image_file = root.find('filename').text
-    elem = root.find('size')
-    image_width = int(elem.find('width').text)
-    image_height = int(elem.find('height').text)
-    elems = root.findall('object')
-    objects = map(parse_object, elems)
-    return (xml_file, image_file, (image_width, image_height), objects)
+    obj = root.findall('object')
+    regions = map(parse_object, obj)
+    return image_file, { 'regions': regions }
 
-def parse_dir(dir_path):
-    items = []
-    files = os.listdir(dir_path)
-    for file_name in files:
-        if os.path.splitext(file_name)[1] == '.xml':
-            items.append(parse_file(os.path.join(dir_path, file_name)))
-    return items
+
+def make_data(list_path, image_dir, annotation_dir, output_dir):
+    with open(list_path) as f:
+        indices = map(lambda x: x.strip(), f.readlines())
+    for index in indices:
+        annotation_path = os.path.join(annotation_dir, '{}.xml'.format(index))
+        image_file, annotation = parse_annotation(annotation_path)
+        image_path = os.path.join(image_dir, image_file)
+        out_image_path = os.path.join(output_dir, image_file)
+        shutil.copy(image_path, out_image_path)
+        base_name = os.path.splitext(image_file)[0]
+        out_annotation_path = os.path.join(output_dir, '{}.json'.format(base_name))
+        with open(out_annotation_path, 'w') as f:
+            json.dump(annotation, f)
+
+
+def main():
+    args = parse_args()
+    input_dir = args.input_dir
+    output_dir = args.output_dir
+
+    annotation_dir = os.path.join(input_dir, 'Annotations')
+    image_dir = os.path.join(input_dir, 'JPEGImages')
+    train_list = os.path.join(input_dir, 'ImageSets', 'Layout', 'train.txt')
+    test_list = os.path.join(input_dir, 'ImageSets', 'Layout', 'val.txt')
+
+    out_train_dir = os.path.join(output_dir, 'train')
+    make_dirs(out_train_dir)
+    make_data(train_list, image_dir, annotation_dir, out_train_dir)
+    out_test_dir = os.path.join(output_dir, 'test')
+    make_dirs(out_test_dir)
+    make_data(test_list, image_dir, annotation_dir, out_test_dir)
+
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('annotation_dir', type=str, help='directory path that contains annotation XML files')
-    parser.add_argument('out_path', type=str, help='output pickle file path')
-    args = parser.parse_args()
-
-    annotations = parse_dir(args.annotation_dir)
-    with open(args.out_path, 'wb') as f:
-        pickle.dump(annotations, f, pickle.HIGHEST_PROTOCOL)
+    main()
